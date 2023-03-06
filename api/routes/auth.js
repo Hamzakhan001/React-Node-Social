bd6fb0ed425398f9f3dcc213f8364580d40a0ac7
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-03-07 02:29:40 +0500

    update profile

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,6 @@
 const router=require("express").Router()
-const User=require("../models/User")
+const User=require("../models/User");
+const bcrypt=require("bcrypt")
 
 
 
@@ -15,17 +16,37 @@ router.get("/register",async (req,res)=>{
 })
 
 router.post("/register",async(req,res)=>{
-	const newUser=new User({
-		userName:req.body.userName,
-		email:req.body.email,
-		password:req.body.password
-	});
-
 	try{
+		const salt=await bcrypt.genSalt(10);
+		const hashedPassword=await bcrypt.hash(req.body.password,salt)
+		const newUser=new User({
+			userName:req.body.userName,
+			email:req.body.email,
+			password:hashedPassword
+		});
 		const user=await newUser.save();
 		res.status(200).json(user)
 	}
 	catch(err){
+		console.error(err)
+	}
+})
+
+router.post("/login",async(req,res)=>{
+	try{
+		const user=await User.findOne({email:req.body.email});
+		if(!user && res.status(404).json("User not found!"));
+
+		const validatePassword=await bcrypt.compare(req.body.password,user.password)
+		if(!validatePassword){
+			res.status(404).json("Password is not correct")
+		}
+		else{
+			res.status(200).json(user)
+		}
+	}
+	catch(err){
+		res.status(500).json(err)
 		console.error(err)
 	}
 })
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,37 @@
 const router=require("express").Router();
+const bcrypt=require("bcrypt")
+const User=require('../models/User')
 
 
 router.get("/",(req,res)=>{
 	res.send("User")
+});
+
+router.put('/:id',async(req,res)=>{
+	if(req.body.id==req.params.id || req.user.isAdmin){
+
+		if(req.body.password){
+			try{
+				const salt=bcrypt.genSalt(10);
+				req.body.password=await bcrypt.hash(req.body.password,hash)
+			}
+			catch(err){return res.status(500).json(err)}
+		}
+		try{
+			const user=await User.findByIdAndUpdate(req.params.id,{
+				$set:req.body
+			});
+			res.status(200).json("Account updated!")
+		}
+		catch(err){
+			console.error(err)
+		}
+	}
+	else{
+		return res.status(403).json("You can update only your account!")
+	}
 })
+
 
 
 
